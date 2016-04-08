c65a208d24a1b8895aa194827d9942e6f29c8a5c
nasimnabavi <nasim.nabavi@scrutinizer-ci.com>
2016-04-08 17:07:07 +0200

    Update index.php

--- img/index.php
+++ img/index.php
@@ -1,19 +1,12 @@
 <?php
-
 class Foo
 {
     public function bar() {
         echo 'baz';
     }
-}
 /**
  * This file is here to mask the directory.
  */
-class Bar
-{
-    /**
-     * This file is here to mask the directory.
-     */
     public function foo() {
         echo 'baz';
     }
