987922e6f6f9abd0da51f8c2496c9be8b6fe3bad
nasimnabavi <nasim.nabavi@scrutinizer-ci.com>
2016-04-08 15:58:51 +0200

    Update index.php

--- img/index.php
+++ img/index.php
@@ -6,3 +6,6 @@ echo "These aren't the droids you're looking for.  Move along.";
 
 echo "These aren't the droids y";
 echo "These aren't the droids y";
+/**
+ * This file is here to mask the directory.
+ */
